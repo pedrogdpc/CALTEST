792f1c0b6937e013685611dc0a94fd013503d9fe
pedrogdpc <pgarcru1@alu.upo.es>
2024-09-29 19:02:26 +0200

    Añadido método mostrarMensaje1 para 'Hola clase de EPD 1'

---
+++ EPD01/P1/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        mostrarMensaje1();
+        mostrarMensaje2();
+    }
+
+    public static void mostrarMensaje1() {
+        System.out.println("Hola clase de EPD 1");
+    }
+
+    public static void mostrarMensaje2() {
+        System.out.println("Esto ha sido hecho en una clase de github");
+    }
+}
